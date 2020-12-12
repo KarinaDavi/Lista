b62d026e21e46724b666e346326de3d56d782b94
KarinaDavi <thedeicider@gmail.com>
2020-12-12 11:04:14 -0300

    Updates

---
+++ precache-manifest.e4200837010ec1d35ece109783c00d45.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e6ca02ae2554038b2596ce0985e080",
+    "url": "/Lista/index.html"
+  },
+  {
+    "revision": "b7fc2c4f1828e728689d",
+    "url": "/Lista/static/js/2.04b38954.chunk.js"
+  },
+  {
+    "revision": "cfdbc6857e57c765481d",
+    "url": "/Lista/static/js/main.c6aea780.chunk.js"
+  },
+  {
+    "revision": "3bba06c2ba47950af328",
+    "url": "/Lista/static/js/runtime-main.8ae26ea3.js"
+  }
+]);
